348725ef5e43d8b02884ee630ff8de8851551f42
Denis Barov <dindin@dindin.ru>
2012-12-25 06:33:35 +0400

    display total for unseen episodes

--- lastunseen.py
+++ lastunseen.py
@@ -7,16 +7,27 @@ import re
 class MyHTMLParser(HTMLParser):
 		tstack = []
 		unseen = []
+		unseen_text = ""
+		epcounthead_opened = False
 		def handle_starttag(self, tag, attrs):
 				self.tstack.append((tag,attrs))
 				if tag == 'a' and self.tstack[-2][1][0][1] == 'myseriesbox':
 						self.unseen.append(attrs[0][1])
+				if tag == 'span' and ('id', 'epcounthead') in attrs:
+						self.epcounthead_opened = True
 		def handle_endtag(self, tag):
+				if self.epcounthead_opened:
+						self.epcounthead_opened = False
 				self.tstack.pop()
 		def handle_startendtag(self, tag, attrs):
 				pass
+		def handle_data(self, data):
+				if self.epcounthead_opened:
+						self.unseen_text =  data
 		def get_unseen(self):
 				return self.unseen
+		def get_unseen_text(self):
+				return self.unseen_text
 
 def lastunseen(seriesName):
 	parser = MyHTMLParser()
@@ -29,7 +40,7 @@ def lastunseen(seriesName):
 def listunseen(retlist=False):
 	unseen = {}
 	unseen_list = []
-	retstr = ""
+	retstr = "\n"
 	parser = MyHTMLParser()
 	page = GetPage.getpage('http://turbofilm.tv/My/Series')["page"]
 	parser.feed(page)
@@ -51,6 +62,7 @@ def listunseen(retlist=False):
 					prefix = ">="
 			else: prefix = "=="
 			retstr+=prefix+" %d\t%s\n" % e
+	retstr += "\n"+"-"*20 + "\n\t%s\n" % parser.get_unseen_text()
 	return retstr
 
 if __name__ == '__main__':
