097eeba04c76a746390eb7b093430d2fa19cb750
Denis Barov <dindin@dindin.ru>
2013-01-24 18:35:06 +0400

    move most of static strings into one file

---
+++ config.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import re
+import os
+
+proto = "https:"
+
+turbofilm_base = 'https://turbofilm.tv'
+
+proto_RE = re.compile("(http:|ftp:|https).*")
+
+srt_urlRE=re.compile('https://(?:.*)/([0-9a-f]{32}).*')
+
+sub_base="http://sub.turbofilm.tv/en/"
+
+login = "dindin@dindin.ru"
+
+passport_auth_url="https://turbofilm.tv/Signin"
+
+headers =  { 'User-Agent' : "Mozilla/5.0 compatible pure-python Turbofilm.tv client" }
+
+cookie_path = os.path.expanduser("~/.turbofilm.tv.cookie")
+
+cdn_base_uri = "https://cdn.turbofilm.tv/"
+cdn_authkey = "A2DC51DE0F8BC1E9"
+
+max_fetch_retry = 5
+
+series_page = "https://turbofilm.tv/My/Series"
+
+watchUrl = 'https://turbofilm.tv/services/epwatch'
+
+sdata_RE = re.compile("http(?:s)+://turbofilm.tv/Watch/(.*)/Season(.*)/Episode(.*)")
+sdata_TMPL = "%s//turbofilm.tv/Watch/%s/Season%s/Episode%s"
+
+wrkdir = "%s/turbofilm" % os.getenv("HOME")
