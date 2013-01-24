9fa57a87305da861937d2957f17371ea7db4e6f0
Denis Barov <dindin@dindin.ru>
2013-01-24 18:38:08 +0400

    use xml.dom.minidome instead of regexp XML parsing

--- xml2srt.py
+++ xml2srt.py
@@ -1,68 +1,52 @@
 #!/usr/bin/env python
+# coding: utf-8
 
-import re, sys, os, time
+import sys, os, time
 import math
 import urllib2
-
-srt_urlRE='http://(?:.*)/([0-9a-f]{32}).*'
-
-sub_base="http://sub.turbofilm.tv/en/"
-bs_RE = ".*<subtitle>.*"
-be_RE = ".*</subtitle>.*"
-start_RE = "<start>([0-9,.]*)</start>"
-stop_RE = "<end>([0-9,.]*)</end>"
-text_RE = "<text>(.*)</text>"
-
-
+import xml.dom.minidom
+import config
 
 def sec_format(strsec):
-    fsec = float(strsec.replace(",","."))
-    hours = int(fsec) / (60**2)
-    minutes = int(fsec - hours*(60**2)) / (60**1)
-    seconds = fsec-(hours*(60**2)+minutes*(60**1))
-    flt = float(fsec) - math.floor(fsec)
-    retstr =  "%02d:%02d:%02d,%d" % (hours, minutes,
-            seconds,int(flt*1000))
-    return retstr
-
-
+		fsec = float(strsec.replace(",","."))
+		hours = int(fsec) / (60**2)
+		minutes = int(fsec - hours*(60**2)) / (60**1)
+		seconds = fsec-(hours*(60**2)+minutes*(60**1))
+		flt = float(fsec) - math.floor(fsec)
+		retstr =  "%02d:%02d:%02d,%d" % (hours, minutes,
+						seconds,int(flt*1000))
+		return unicode(retstr)
 
 def main(argv):
-    infile = argv[1]
-    srt_match = re.match(srt_urlRE, infile)
-
-    if srt_match:
-        infile=sub_base + srt_match.groups()[0]
-    fetch_sub(infile)
-
+		infile = argv[1]
+		srt_match = config.srt_urlRE.match(infile)
+		if srt_match:
+				infile = config.sub_base + srt_match.groups()[0]
+		fetch_sub(infile)
 
 def fetch_sub(infile, srt_location="/Users/dindin/tmp/lastflash.srt"):
-    print infile
-    f = urllib2.urlopen(infile)
-    srt = file(srt_location, "w")
-    line = True
-    counter = 0
-    while line:
-      line = f.readline()
-      if re.match(bs_RE,line):
-          counter += 1
-          block = "" 
-          while not re.match(be_RE,line):
-              line = f.readline()
-              block += line.replace('\n',' ')
-              block = block.replace('\r', '')
-          start = re.findall(start_RE,block)[0]
-          stop = re.findall(stop_RE,block)[0]
-          try:
-              text = re.findall(text_RE,block)[0]
-              text = text.replace("&lt;i&gt;", "<i>")
-              text = text.replace("&lt;/i&gt;", "</i>")
-          except IndexError: continue
+		if not config.proto_RE.match(infile):
+				infile = config.proto + infile
+		print infile
+		f = urllib2.urlopen(infile)
+		srt = file(srt_location, "w")
+		counter = 0
 
-          srt.write("%s\n" % (counter))
-          srt.write("%s --> %s\n" % (sec_format(start), sec_format(stop)))
-          srt.write("%s\n\n" % text.replace("\r ","\n"))
-    srt.close()
+		data = unicode(f.read(), errors='ignore') #.decode("utf-8-sig").strip()
+		print type(data)
+		dom = xml.dom.minidom.parseString(data)
+		for e in dom.getElementsByTagName("subtitle"):
+				try: 
+						e.getElementsByTagName("start")[0].childNodes[0].data
+						counter+=1
+						srt.write(u"%s\n" % (counter))
+						srt.write(u"%s --> %s\n" % (
+							sec_format(e.getElementsByTagName("start")[0].childNodes[0].data),
+							sec_format(e.getElementsByTagName("end")[0].childNodes[0].data)))
+						srt.write("%s\n\n" %
+								e.getElementsByTagName("text")[0].childNodes[0].data)
+				except IndexError: pass
+		srt.close()
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv))
+		sys.exit(main(sys.argv))
