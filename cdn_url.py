204213fabf55030ec706c3066087792565f920fb
Denis Barov <dindin@dindin.ru>
2013-08-27 16:05:45 +0400

    add ssn_url function (SeriesSeasonEpisode)

--- cdn_url.py
+++ cdn_url.py
@@ -4,6 +4,9 @@ from hashlib import sha1
 from random import random
 import config
 
+def ssn_url(t_name, season, episode):
+		return config.sdata_TMPL % (config.proto, t_name, season, episode)
+
 def cdn_url(iasid, eid, current_source, offset, lang):
 		rseed = sha1(iasid + str(random())).hexdigest()
 		skey = sha1(rseed + eid + config.cdn_authkey).hexdigest()
