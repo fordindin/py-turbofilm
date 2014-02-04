b051db54193fe1b1253e2ca6c30e578b1705bae5
Denis Barov <dindin@dindin.ru>
2014-02-05 01:47:58 +0400

    config fixes

--- config.py
+++ config.py
@@ -7,40 +7,44 @@ import os
 proto = "https:"
 
 
-turbofilm_base = 'https://turbofilm.tv'
+turbofilm_base = 'https://turbik.tv'
 
 proto_RE = re.compile("(http:|ftp:|https).*")
 
 srt_urlRE=re.compile('https://(?:.*)/([0-9a-f]{32}).*')
 
-sub_base="http://sub.turbofilm.tv/en/"
+sub_base="http://sub.turbik.tv/en/"
 
 login = "dindin@dindin.ru"
 
-auth_url="https://turbofilm.tv/Signin"
+auth_url="https://turbik.tv/Signin"
 
 headers =  { 'User-Agent' : "Mozilla/5.0 compatible pure-python Turbofilm.tv client" }
 
-cookie_path = os.path.expanduser("~/.turbofilm.tv.cookie")
+cookie_path = os.path.expanduser("~/.turbik.tv.cookie")
 
-cdn_base_uri = "https://cdn.turbofilm.tv/"
+cdn_base_uri = "https://cdn.turbik.tv/"
 cdn_authkey = "A2DC51DE0F8BC1E9"
 
 max_fetch_retry = 5
 
 wait_time = 1
 
-series_page = "https://turbofilm.tv/My/Series"
+series_page = "https://turbik.tv/My/Series"
 
-watchUrl = 'https://turbofilm.tv/services/epwatch'
+watchUrl = 'https://turbik.tv/services/epwatch'
 
-sdata_RE = re.compile("http(?:s)+://turbofilm.tv/Watch/(.*)/Season(.*)/Episode(.*)")
-sdata_TMPL = "%s//turbofilm.tv/Watch/%s/Season%s/Episode%s"
+sdata_RE = re.compile("http(?:s)+://turbik.tv/Watch/(.*)/Season(.*)/Episode(.*)")
+sdata_TMPL = "%s//turbik.tv/Watch/%s/Season%s/Episode%s"
 
 wrkdir = "%s/turbofilm" % os.getenv("HOME")
+
+offline_dir = os.path.join(wrkdir, "offline")
 
 socks_enable = False
 socks_ip = "199.231.185.97"
 socks_port = 1080
 #socks_ip = "127.0.0.1"
 #socks_port = 9050
+#myresolver="resolv.conf"
+myresolver=False
